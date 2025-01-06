90c73daa226d9a7393eddca6176dcd5aaba5328e
Scoccc <97665263+Scoccc@users.noreply.github.com>
2025-01-06 16:27:44 +0100

    updated lexer and parser

---
+++ imp/fun/lib/main.ml
@@ -0,0 +1,151 @@
+open Types
+open Ast
+
+let parse (s : string) : cmd =
+  let lexbuf = Lexing.from_string s in
+  let ast = Parser.prog Lexer.read lexbuf in
+  ast
+;;
+
+
+let apply st x =
+  match topenv st x with
+  | IVar l -> getmem st l
+  | BVar l -> getmem st l
+
+let rec eval_expr (st : state) (e : expr) : memval =
+  match e with
+  | True -> Bool true
+  | False -> Bool false
+  | Var x -> apply st x
+  | Const n -> Int n
+  | Not e1 -> 
+    (
+      match eval_expr st e1 with
+      | Bool b -> Bool (not b)
+      | _ -> failwith "Type error: Not expects a boolean"
+    )
+  | And (e1, e2) -> 
+    (
+      match (eval_expr st e1, eval_expr st e2) with
+      | Bool b1, Bool b2 -> Bool (b1 && b2)
+      | _ -> failwith "Type error: And expects booleans"
+    )
+  | Or (e1, e2) -> 
+    (
+      match (eval_expr st e1, eval_expr st e2) with
+      | Bool b1, Bool b2 -> Bool (b1 || b2)
+      | _ -> failwith "Type error: Or expects booleans"
+    )
+  | Add (e1, e2) -> 
+    (
+      match (eval_expr st e1, eval_expr st e2) with
+      | Int n1, Int n2 -> Int (n1 + n2)
+      | _ -> failwith "Type error: Add expects integers"
+    )
+  | Sub (e1, e2) -> 
+    (
+      match (eval_expr st e1, eval_expr st e2) with
+      | Int n1, Int n2 -> Int (n1 - n2)
+      | _ -> failwith "Type error: Sub expects integers"
+    )
+  | Mul (e1, e2) -> 
+    (
+      match (eval_expr st e1, eval_expr st e2) with
+      | Int n1, Int n2 -> Int (n1 * n2)
+      | _ -> failwith "Type error: Mul expects integers"
+    )
+  | Eq (e1, e2) -> 
+    (
+      match (eval_expr st e1, eval_expr st e2) with
+      | Int n1, Int n2 -> Bool (n1 = n2)
+      | _ -> failwith "Type error: Eq expects integers"
+    )
+  | Leq (e1, e2) -> 
+    (
+      match (eval_expr st e1, eval_expr st e2) with
+      | Int n1, Int n2 -> Bool (n1 <= n2)
+      | _ -> failwith "Type error: Leq expects integers"
+    )
+;;
+
+let rec eval_decl (st : state) (dls : decl list) : state =
+  match dls with
+  | [] -> st
+  | IntVar(x)::dls' ->
+      let loc = getloc st in
+      let env = bind_env (topenv st) x (IVar loc) in
+      let st' = setenv (setloc st (loc + 1)) (env :: getenv st) in
+      eval_decl st' dls'
+  | BoolVar(x)::dls' ->
+      let loc = getloc st in
+      let env = bind_env (topenv st) x (BVar loc) in
+      let st' = setenv (setloc st (loc + 1)) (env :: getenv st) in
+      eval_decl st' dls'
+;;
+
+let rec trace1 (c : conf) : conf =
+  match c with
+  | St _ -> raise NoRuleApplies
+  | Cmd (Skip, st) -> St st
+  | Cmd (Assign (x, e), st) -> 
+  (
+    match eval_expr st e with
+    | Bool v -> 
+      (
+        match topenv st x with 
+        | BVar l -> St (setmem st(bind_mem (getmem st) l (Bool v)))
+        | _ -> failwith "Cannot assign Bool to Int var"
+      )
+      | Int v -> 
+        (
+          match topenv st x with 
+          | IVar l -> St (setmem st(bind_mem (getmem st) l (Int v)))
+          | _ -> failwith "Cannot assign Int to Bool var"
+        )
+  )
+  | Cmd (Seq (c1, c2), st) -> 
+      (
+        match trace1 (Cmd (c1, st)) with
+        | St st' -> Cmd (c2, st')
+        | Cmd (c1', st') -> Cmd (Seq (c1', c2), st')
+      )
+  | Cmd (If (e, c1, c2), st) -> 
+      (
+        match eval_expr st e with
+        | Bool true -> Cmd (c1, st)
+        | Bool false -> Cmd (c2, st)
+        | _ -> failwith "Type error: If condition must be boolean"
+      )
+  | Cmd (While (e, c), st) -> 
+      (
+        match eval_expr st e with
+        | Bool true -> Cmd (Seq (c, While (e, c)), st)
+        | Bool false -> St st
+        | _ -> failwith "Type error: While condition must be boolean"
+      )
+  | Cmd (Decl (dls, c), st) ->
+    (
+      Cmd (c, eval_decl st dls)
+    )
+  | Cmd (Block c, st) -> 
+  (
+    match trace1 (Cmd(c, st)) with
+    | St st' -> St(setenv st'(popenv st))
+    | Cmd(c',st') -> Cmd(Block c', st')
+  )
+  ;;
+
+let rec trace_rec n t =
+  if n <= 0 then 
+    [t]
+  else
+    try 
+      let t' = trace1 t in
+      t :: (trace_rec (n-1) t')
+    with NoRuleApplies -> [t]
+  ;;
+
+let trace (n : int) (c : cmd) : conf list = trace_rec n (Cmd(c, make_state [bottom_env] bottom_mem 0))
+;;
+  
---
+++ imp/fun/lib/parser.mly
@@ -0,0 +1,85 @@
+%{
+open Ast
+%}
+
+%token TRUE
+%token FALSE
+%token<string> IDE
+%token<string> CONST
+%token NOT
+%token AND
+%token OR
+%token ADD 
+%token SUB 
+%token MUL 
+%token EQ 
+%token LEQ 
+%token SKIP 
+%token ASSIGN
+%token SEQ 
+%token IF 
+%token THEN 
+%token ELSE
+%token WHILE
+%token DO
+%token LPAREN
+%token RPAREN
+%token LBLOCKPAREN
+%token RBLOCKPAREN
+%token INTEGER
+%token BOOLEAN
+%token FUNCTION
+%token RETURN
+%token EOF
+
+%left SEQ
+%left OR AND
+%nonassoc NOT
+%left EQ LEQ
+%left ADD SUB
+%left MUL
+%nonassoc DO ELSE
+
+%start <cmd> prog
+
+%%
+
+prog:
+  | c = cmd; EOF { c }
+;
+
+expr:
+  | TRUE { True }
+  | FALSE { False }
+  | v = IDE { Var(v) }
+  | n = CONST { Const(int_of_string n) }
+  | NOT; e = expr { Not(e) }
+  | e1 = expr; AND; e2 = expr { And(e1,e2) }
+  | e1 = expr; OR; e2 = expr { Or(e1,e2) }
+  | e1 = expr; ADD; e2 = expr { Add(e1,e2) }
+  | e1 = expr; SUB; e2 = expr { Sub(e1,e2) }
+  | e1 = expr; MUL; e2 = expr { Mul(e1,e2) }
+  | e1 = expr; EQ; e2 = expr { Eq(e1,e2) }
+  | e1 = expr; LEQ; e2 = expr { Leq(e1,e2) }
+  | name = IDE; param = expr; { Call(name, param) }
+  | LPAREN; e = expr; RPAREN { e }
+;
+
+decl:
+  | INTEGER; v = IDE { IntVar(v) }
+  | BOOLEAN; v = IDE { BoolVar(v) }
+  | FUNCTION; name = IDE; param = IDE; LBLOCKPAREN; body = cmd; RETURN; ret = expr; RBLOCKPAREN {Fun(name, param, body, ret)}
+
+decls:
+ | { [] }
+ | d = decl; SEQ; ds = decls { d :: ds }
+
+cmd:
+ | SKIP { Skip }
+ | v = IDE; ASSIGN; e = expr { Assign(v, e) }
+ | c1 = cmd; SEQ; c2 = cmd { Seq(c1,c2) }
+ | IF; e = expr; THEN; c1 = cmd; ELSE; c2 = cmd { If(e,c1,c2) }
+ | WHILE; e = expr; DO; c = cmd { While(e,c) }
+ | LBLOCKPAREN; ds = decls; c = cmd; RBLOCKPAREN {Decl(ds, c)}
+ | LPAREN; c = cmd; RPAREN { c }
+
